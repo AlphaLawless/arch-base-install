bd0803fd36d61c50e8171a3f098fbefb85921d94
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2021-09-20 17:38:24 -0300

    upload: create script for install bspwm

---
+++ bspwm/bspwm-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo pacman -Syy
+
+# git clone https://aur.archlinux.org/paru-bin
+# cd paru/bin
+# makepkg -si --noconfirm
+
+# paru -S ly-git
+# sudo systemctl enable ly.service
+
+# git clone https://aur.archlinux.org/pikaur.git
+# cd pikaur/
+# makepkg -si --noconfirm
+
+# pikaur -S --noconfirm polybar
+# pikaur -S --noconfirm nerd-fonts-iosevka
+# pikaur -S --noconfirm tt-icomoon-feather
+
+# pikaur -S --noconfirm system76-power
+# sudo systemctl enable --now system76-power
+# sudo system76-power graphics integrated
+# pikaur -S --noconfirm gnome-shell-extension-system76-power-git
+# pikaur -S --noconfirm auto-cpufreq
+# sudo systemctl enable --now auto-cpufreq
+
+echo "\n[!] MAIN PACKAGES"
+
+sleep 5
+
+sudo pacman -S --noconfirm xorg xorg-xrandr bspwm sxhkd dunst rofi dmenu firefox kitty picom unclutter feh lightdm light-locker nautilus arandr pulseaudio-alsa pavucontrol arc-gtk-theme arc-icon-theme vlc xclip pacman-contrib
+
+echo "\n[*] INSTALLING FONTS...\n"
+
+sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts font-bh-ttf ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts
+
+sudo systemctl enable lightdm
+
+mkdir -p .config/{bspwm,sxhkd,dunst}
+
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT\e[0m"
