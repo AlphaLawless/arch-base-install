bffac4dfa4567796003a48832093316e611e56d1
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2021-09-23 13:48:44 -0300

    bugfix: indentation

--- bspwm/polybar/install.sh
+++ bspwm/polybar/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Author: adi1090x
+# repository: https://github.com/adi1090x/polybar-themes
+# Install script for polybar themes
 
 # DIRS
 DIR=`pwd`
@@ -7,60 +10,59 @@ POLYBARDIR="$HOME/.config/polybar"
 
 # INSTALL FONTS
 install_fonts() {
-    echo -e "\n[*] Installing fonts..."
-    if [[ -d "$FONTSDIR" ]]; then
-        cp -rf $DIR/fonts/* "$FONTSDIR"
-    else
-        mkdir -p "$FONTSDIR"
-        cp -rf $DIR/fonts/* "$FONTSDIR"
-    fi
+	echo -e "\n[*] Installing fonts..."
+	if [[ -d "$FONTSDIR" ]]; then
+		cp -rf $DIR/fonts/* "$FONTSDIR"
+	else
+		mkdir -p "$FONTSDIR"
+		cp -rf $DIR/fonts/* "$FONTSDIR"
+	fi
 }
 
 # INSTALL THEMES
 install_themes() {
-    if [[ -d "$POLYBARDIR" ]]; then
-        echo -e "[*] Creating a backup of your polybar config..."
-        mv "$POLYBARDIR" "${POLYBARDIR}.old"
-        { mkdir -p "$POLYBARDIR"; cp -rf $DIR/$STYLE/* "$POLYBARDIR"; }
-    else
-        { mkdir -p "$POLYBARDIR"; cp -rf $DIR/$STYLE/* "$POLYBARDIR"; }
-    fi
-
-    if [[ -f "$POLYBARDIR/launch.sh" ]]; then
-        echo -e "[*] Sucessfully installed.\n"
-        exit 0
-    else
-        echo -e "[!] Failed to install.\n"
-        exit 1
-    fi
+	if [[ -d "$POLYBARDIR" ]]; then
+		echo -e "[*] Creating a backup of your polybar configs..."
+		mv "$POLYBARDIR" "${PDIR}.old"
+		{ mkdir -p "$POLYBARDIR"; cp -rf $DIR/$STYLE/* "$POLYBARDIR"; }
+	else
+		{ mkdir -p "$POLYBARDIR"; cp -rf $DIR/$STYLE/* "$POLYBARDIR"; }
+	fi
+	if [[ -f "$POLYBARDIR/launch.sh" ]]; then
+		echo -e "[*] Successfully Installed.\n"
+		exit 0
+	else
+		echo -e "[!] Failed to install.\n"
+		exit 1
+	fi
 }
 
 # MAIN
 main() {
-    clear
-    cat <<- EOF
-        [*] Installing Polybar Themes...
+	clear
+	cat <<- EOF
+		[*] Installing Polybar Themes...
 
-        [*] Choose Style -
-        [1] Simple
-        [2] Bitmap
+		[*] Choose Style -
+		[1] Simple
+		[2] Bitmap
 
-    EOF
+	EOF
 
-    read -p "[?] Select Option : "
+	read -p "[?] Select Option : "
 
-    if [[ $REPLY == "1" ]]; then
-        STYLE='simple'
-        install_fonts
-        install_themes
-    elif [[  $REPLY == '2']]; then
-        STYLE='bitmap'
-        install_fonts
-        install_themes
-    else
-        echo -e "\n[!] Invalid Option, Exiting...\n"
-        exit 1
-    fi
+	if [[ $REPLY == "1" ]]; then
+		STYLE='simple'
+		install_fonts
+		install_themes
+	elif [[ $REPLY == "2" ]]; then
+		STYLE='bitmap'
+		install_fonts
+		install_themes
+	else
+		echo -e "\n[!] Invalid Option, Exiting...\n"
+		exit 1
+	fi
 }
 
 main
