651017d20efa89e41c532b97c4a2bf98ed538a18
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2021-09-26 01:27:42 -0300

    bugfix: EOF

--- scripts/languages/english.sh
+++ scripts/languages/english.sh
@@ -10,45 +10,45 @@ packages() {
         [2] Soft Packages
     EOF
 
-  read -p "[?] Select Option, typing 1 or 2: "
+    read -p "[?] Select Option, typing 1 or 2: "
 
-  if [[ $REPLY == "1" ]]; then
-    pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant xdg-user-dirs xdg-utils wireless_tools dosfstools mtools linux-headers avahi openssh openbsd-netcat ipset firewalld nss-mdns dnsutils vde2 nfs-utils bash-completion reflector sof-firmware dnsmasq pulseaudio alsa-utils virt-manager qemu qemu-arch-extra edk2-ovmf acpi acpi_call acpid cups ntfs-3g terminus-font
-    sleep 5 ; clear ; lsblk
-    echo -e "Enter your partition name for grub installation, use the table above. e.g. /dev/sdx: "
-    read partition_name
-    grub-install --target=i386-pc ${partition_name}
-    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+    if [[ $REPLY == "1" ]]; then
+        pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant xdg-user-dirs xdg-utils wireless_tools dosfstools mtools linux-headers avahi openssh openbsd-netcat ipset firewalld nss-mdns dnsutils vde2 nfs-utils bash-completion reflector sof-firmware dnsmasq pulseaudio alsa-utils virt-manager qemu qemu-arch-extra edk2-ovmf acpi acpi_call acpid cups ntfs-3g terminus-font
+        sleep 5 ; clear ; lsblk
+        echo -e "Enter your partition name for grub installation, use the table above. e.g. /dev/sdx: "
+        read partition_name
+        grub-install --target=i386-pc ${partition_name}
+        echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
 
-    systemctl enable NetworkManager
-    systemctl enable sshd
-    systemctl enable reflector.timer
-    systemctl enable cups.service
-    systemctl enable avahi-daemon
-    systemctl enable firewalld
-    systemctl enable libvirtd
-    systemctl enable acpid
-    systemctl enable fstrim.timer
-  elif [[ $REPLY == "2" ]]; then
-    pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant wireless_tools dosfstools mtools openssh reflector firewalld cups virt-manager qemu qemu-arch-extra edk2-ovmf pulseaudio alsa-utils
-    sleep 5 ; clear ; lsblk
-    echo -e "Enter your partition name for grub installation, use the table above. e.g. /dev/sdx: "
-    read partition_name
-    grub-install --target=i386-pc ${partition_name}
-    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+        systemctl enable NetworkManager
+        systemctl enable sshd
+        systemctl enable reflector.timer
+        systemctl enable cups.service
+        systemctl enable avahi-daemon
+        systemctl enable firewalld
+        systemctl enable libvirtd
+        systemctl enable acpid
+        systemctl enable fstrim.timer
+    elif [[ $REPLY == "2" ]]; then
+        pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant wireless_tools dosfstools mtools openssh reflector firewalld cups virt-manager qemu qemu-arch-extra edk2-ovmf pulseaudio alsa-utils
+        sleep 5 ; clear ; lsblk
+        echo -e "Enter your partition name for grub installation, use the table above. e.g. /dev/sdx: "
+        read partition_name
+        grub-install --target=i386-pc ${partition_name}
+        echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
 
-    systemctl enable NetworkManager
-    systemctl enable sshd
-    systemctl enable reflector.timer
-    systemctl enable cups.service
-    systemctl enable firewalld
-    systemctl enable libvirtd
-  else
-    echo "[!] Invalid option!"
-    sleep 2 ; clear ; packages
-  fi
+        systemctl enable NetworkManager
+        systemctl enable sshd
+        systemctl enable reflector.timer
+        systemctl enable cups.service
+        systemctl enable firewalld
+        systemctl enable libvirtd
+    else
+        echo "[!] Invalid option!"
+        sleep 2 ; clear ; packages
+    fi
 }
 packages
 
@@ -92,48 +92,48 @@ videoDriverInstall(){
 videoDriverInstall
 
 createPassword() {
-  echo -e "Create a password for username: \n"
-  unset password
-  while IFS= read -r -s -n1 pass; do
-    if [[ -z $pass ]]; then
-      echo
-      break
+    echo -e "Create a password for username: \n"
+    unset password
+    while IFS= read -r -s -n1 pass; do
+        if [[ -z $pass ]]; then
+        echo
+        break
+        else
+        echo -n '*'
+        password+=$pass
+        fi
+    done
+    echo "Please, confirm it typing again: "
+    unset confirm
+    while IFS= read -r -s -n1 conf; do
+        if [[ -z $conf ]]; then
+        echo
+        break
+        else
+        echo -n '*'
+        confirm+=$conf
+        fi
+    done
+    if [[ $password == $confirm ]]; then
+        echo -e "[!] All Right! Continuing...\n"
+        sleep 2
     else
-      echo -n '*'
-      password+=$pass
+        sleep 2 ; clear
+        echo -e "[!] Wrong, please do it again\n"
+        createPassword
     fi
-  done
-  echo "Please, confirm it typing again: "
-  unset confirm
-  while IFS= read -r -s -n1 conf; do
-    if [[ -z $conf ]]; then
-      echo
-      break
-    else
-      echo -n '*'
-      confirm+=$conf
-    fi
-  done
-  if [[ $password == $confirm ]]; then
-    echo -e "[!] All Right! Continuing...\n"
-    sleep 2
-  else
-    sleep 2 ; clear
-    echo -e "[!] Wrong, please do it again\n"
-    createPassword
-  fi
 }
 
 createUser() {
-  clear
-  echo -e "Create a username: "
-  read username
-  createPassword
-  useradd -m ${username}
-  echo ${username}:${password} | chpasswd
-  usermod -aG libvirt ${username}
+    clear
+    echo -e "Create a username: "
+    read username
+    createPassword
+    useradd -m ${username}
+    echo ${username}:${password} | chpasswd
+    usermod -aG libvirt ${username}
 
-  # Adding user in SUDOERS
-  echo "${username} ALL=(ALL) ALL" >> /etc/sudoers.d/${username}
+    # Adding user in SUDOERS
+    echo "${username} ALL=(ALL) ALL" >> /etc/sudoers.d/${username}
 }
 createUser
--- scripts/languages/portuguese.sh
+++ scripts/languages/portuguese.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 packages() {
-  clear
-  cat <<- EOF
-    [*] Quais os pacotes você quer instalar?
+    clear
+    cat <<- EOF
+        [*] Quais os pacotes você quer instalar?
 
-    [*] Escolha um -
-    [1] Pacotes principais
-    [2] Pacotes essenciais
-  EOF
+        [*] Escolha um -
+        [1] Pacotes principais
+        [2] Pacotes essenciais
+    EOF
 
-  read -p "[?] Selecione a opção, digitando 1 ou 2: "
+    read -p "[?] Selecione a opção, digitando 1 ou 2: "
 
-  if [[ $REPLY == "1" ]]; then
-    pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant xdg-user-dirs xdg-utils wireless_tools dosfstools mtools linux-headers avahi openssh openbsd-netcat ipset firewalld nss-mdns dnsutils vde2 nfs-utils bash-completion reflector sof-firmware dnsmasq pulseaudio alsa-utils virt-manager qemu qemu-arch-extra edk2-ovmf acpi acpi_call acpid cups ntfs-3g terminus-font
-    sleep 5 ; clear ; lsblk
-    echo -e "Digite o nome da sua partição para instalar o grub, use a tabela acima. e.g. /dev/sdx: "
-    read partition_name
-    grub-install --target=i386-pc ${partition_name}
-    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+    if [[ $REPLY == "1" ]]; then
+        pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant xdg-user-dirs xdg-utils wireless_tools dosfstools mtools linux-headers avahi openssh openbsd-netcat ipset firewalld nss-mdns dnsutils vde2 nfs-utils bash-completion reflector sof-firmware dnsmasq pulseaudio alsa-utils virt-manager qemu qemu-arch-extra edk2-ovmf acpi acpi_call acpid cups ntfs-3g terminus-font
+        sleep 5 ; clear ; lsblk
+        echo -e "Digite o nome da sua partição para instalar o grub, use a tabela acima. e.g. /dev/sdx: "
+        read partition_name
+        grub-install --target=i386-pc ${partition_name}
+        echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
 
-    systemctl enable NetworkManager
-    systemctl enable sshd
-    systemctl enable reflector.timer
-    systemctl enable cups.service
-    systemctl enable avahi-daemon
-    systemctl enable firewalld
-    systemctl enable libvirtd
-    systemctl enable acpid
-    systemctl enable fstrim.timer
-  elif [[ $REPLY == "2" ]]; then
-    pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant wireless_tools dosfstools mtools openssh reflector firewalld cups virt-manager qemu qemu-arch-extra edk2-ovmf pulseaudio alsa-utils
-    sleep 5 ; clear ; lsblk
-    echo -e "Digite o nome da sua partição para instalar o grub, use a tabela acima. e.g. /dev/sdx: "
-    read partition_name
-    grub-install --target=i386-pc ${partition_name}
-    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+        systemctl enable NetworkManager
+        systemctl enable sshd
+        systemctl enable reflector.timer
+        systemctl enable cups.service
+        systemctl enable avahi-daemon
+        systemctl enable firewalld
+        systemctl enable libvirtd
+        systemctl enable acpid
+        systemctl enable fstrim.timer
+    elif [[ $REPLY == "2" ]]; then
+        pacman -S grub base-devel os-prober networkmanager network-manager-applet dialog wpa_supplicant wireless_tools dosfstools mtools openssh reflector firewalld cups virt-manager qemu qemu-arch-extra edk2-ovmf pulseaudio alsa-utils
+        sleep 5 ; clear ; lsblk
+        echo -e "Digite o nome da sua partição para instalar o grub, use a tabela acima. e.g. /dev/sdx: "
+        read partition_name
+        grub-install --target=i386-pc ${partition_name}
+        echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
 
-    systemctl enable NetworkManager
-    systemctl enable sshd
-    systemctl enable reflector.timer
-    systemctl enable cups.service
-    systemctl enable firewalld
-    systemctl enable libvirtd
-  else
-    echo "[!] Opção inválida!"
-    sleep 2 ; clear ; packages
-  fi
+        systemctl enable NetworkManager
+        systemctl enable sshd
+        systemctl enable reflector.timer
+        systemctl enable cups.service
+        systemctl enable firewalld
+        systemctl enable libvirtd
+    else
+        echo "[!] Opção inválida!"
+        sleep 2 ; clear ; packages
+    fi
 }
 packages
 
@@ -92,49 +92,49 @@ videoDriverInstall(){
 videoDriverInstall
 
 createPassword() {
-  echo -e "Digite a senha para o usuário: \n"
-  unset password
-  while IFS= read -r -s -n1 pass; do
-    if [[ -z $pass ]]; then
-      echo
-      break
+    echo -e "Digite a senha para o usuário: \n"
+    unset password
+    while IFS= read -r -s -n1 pass; do
+        if [[ -z $pass ]]; then
+        echo
+        break
+        else
+        echo -n '*'
+        password+=$pass
+        fi
+    done
+    echo "Por favor, confirme novamente: "
+    unset confirm
+    while IFS= read -r -s -n1 conf; do
+        if [[ -z $conf ]]; then
+        echo
+        break
+        else
+        echo -n '*'
+        confirm+=$conf
+        fi
+    done
+    if [[ $password == $confirm ]]; then
+        echo -e "[!] Tudo certo! Continuando...\n"
+        sleep 2
     else
-      echo -n '*'
-      password+=$pass
+        sleep 2 ; clear
+        echo -e "[!] Errado, por favor faça novamente\n"
+        createPassword
     fi
-  done
-  echo "Por favor, confirme novamente: "
-  unset confirm
-  while IFS= read -r -s -n1 conf; do
-    if [[ -z $conf ]]; then
-      echo
-      break
-    else
-      echo -n '*'
-      confirm+=$conf
-    fi
-  done
-  if [[ $password == $confirm ]]; then
-    echo -e "[!] Tudo certo! Continuando...\n"
-    sleep 2
-  else
-    sleep 2 ; clear
-    echo -e "[!] Errado, por favor faça novamente\n"
-    createPassword
-  fi
 }
 
 createUser() {
-  clear
-  echo -e "Crie um usuário: "
-  read username
-  createPassword
-  useradd -m ${username}
-  echo ${username}:${password} | chpasswd
-  usermod -aG libvirt ${username}
+    clear
+    echo -e "Crie um usuário: "
+    read username
+    createPassword
+    useradd -m ${username}
+    echo ${username}:${password} | chpasswd
+    usermod -aG libvirt ${username}
 
-  # Adding user in SUDOERS
-  echo "${username} ALL=(ALL) ALL" >> /etc/sudoers.d/${username}
+    # Adding user in SUDOERS
+    echo "${username} ALL=(ALL) ALL" >> /etc/sudoers.d/${username}
 }
 createUser
 
